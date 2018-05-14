cdc3e0d32c10f57779f8d97c23b475401bf402d4
zhangbang <zhangbang@meituan.com>
2018-05-14 10:32:39 +0800

    添加redux

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Routes from '../client/Routes';
+import { Provider } from 'react-redux';
 
-export default req => {
-    const content = renderToString(<StaticRouter location={req.path} context={{}}><Routes/></StaticRouter>);
+export default (req, store) => {
+    const content = renderToString(
+        <Provider store={store}>
+            <StaticRouter location={req.path} context={{}}>
+                <Routes/>
+            </StaticRouter>
+        </Provider>
+        );
     const html = `
         <html>
             <head></head>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    const html = renderer(req);
+    const store = createStore();
+    const html = renderer(req, store);
     res.send(html);
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('listening on port 3000');
 })
