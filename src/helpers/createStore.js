12b48cc2167e5f7fc04d79110be76b58b276e7dd
Demon Monk <antdude@126.com>
2018-05-16 23:07:08 +0800

    渲染服务器增加代理 渲染服务器发api请求时携带cookie

--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../client/reducers';
+import axios from 'axios';
 
-export default () => {
-    return createStore(reducers, {}, applyMiddleware(thunk));
+export default (req) => {
+    const axiosInstance = axios.create({
+        baseURL: 'http://react-ssr-api.herokuapp.com',
+        headers: {
+            cookie: req.get('cookie') || ''
+        }
+    })
+    return createStore(reducers, {}, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 }
