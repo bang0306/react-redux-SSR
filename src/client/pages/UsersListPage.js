46781f9bc7d444e75773de01746c1f983f418fe4
Demon Monk <antdude@126.com>
2018-05-18 21:35:26 +0800

    sth about SEO

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 import { fetchUsers } from '../actions';
 
 class UsersList extends Component {
@@ -8,11 +9,23 @@ class UsersList extends Component {
         this.props.fetchUsers();
     }
 
+    renderHeaders() {
+        return (
+            <Helmet>
+                <title>{`${this.props.users.length} users loaded`}</title>
+                <meta property="og:title" content="a list of titles" />
+            </Helmet>
+        )
+    }
+
     render() {
         return (
-            <ul>
-                {this.props.users.map((user) => <li key={user.id}>{user.name}</li>)}
-            </ul>
+            <div>
+                { this.renderHeaders() }
+                <ul>
+                    {this.props.users.map((user) => <li key={user.id}>{user.name}</li>)}
+                </ul>
+            </div>
         );
     }
 }
